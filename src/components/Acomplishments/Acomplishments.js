09dedd7862714163165ed3a225c91c0801dfc6cf
Charles Nwoye <charlesnwoye2@gmail.com>
2022-06-26 10:48:56 +0100

    added responsiveness

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -7,7 +7,7 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 const data = [
   { text: 'Hackerrank 30 days of code', image: '/images/30_days.jpeg' },
   { text: 'Hackerrank problem solving', image: '/images/problem_solving.jpeg' },
-  { text: 'Hackerrank python programming', image: '/images/python_gold.png' },
+  { text: 'Hackerrank python', image: '/images/python_gold.png' },
 ];
 
 const Acomplishments = () => (
@@ -16,7 +16,7 @@ const Acomplishments = () => (
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
-          <img style={{ height: '7rem', width: '20rem' }} src={card.image} />
+          <Img style={{ height: '65%', width: '100%' }} src={card.image} />
           <BoxText>{card.text}</BoxText>
         </Box>
       ))}
